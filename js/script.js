32b3589830b1ddac4b5016d01bb2d17890ef8512
Todd Foster <todd.foster@logos.com>
2012-05-19 08:18:02 -0700

    Beginning js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
-/* Author:
-
+/* Author: Todd Foster
 */
 
+/* global boidem */
+boidem = {};
 
+boidem.adlessCalc = (function() {
+  var loadFinished = (function() {
+      console.log("loadFinished");
+      alert("loadfinished");
+  });
 
+  return {
+    onDocumentReady:function() {
+		loadFinished();
+    }
+  }
+}());
 
+$(document).ready(boidem.adlessCalc.onDocumentReady);
 
