08ad3f343c63c5f61c6595a5eb905411462189ad
Todd Foster <todd.foster@logos.com>
2012-05-19 22:51:11 -0700

    Implement numerous operations

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ o font size for display
 o web-app, offline manifest
 o icon
 o unicode multiply,divide symbols
+o test, deal with floating point precision problems (return 1 instead of 0.9999...)
+o display/clear error conditions
 */
 
 /* global boidem */
@@ -18,30 +20,30 @@ boidem.adlessCalc = (function() {
 	var display;
 	var buttons = [];
 	var buttonDefinitions = [
-		{ 'symbol':'=', 'class':'greenButton', 'action':function() { console.log("= unimplemented"); } },
-		{ 'symbol':'X', 'class':'greenButton', 'action':function() { console.log("X unimplemented"); } },
-		{ 'symbol':'/', 'class':'greenButton', 'action':function() { console.log("/ unimplemented"); } },
-		{ 'symbol':'-', 'class':'greenButton', 'action':function() { console.log("- unimplemented"); } },
-		{ 'symbol':'+', 'class':'greenButton', 'action':function() { console.log("+ unimplemented"); } },
-		{ 'symbol':'+', 'class':'greenButton', 'action':function() { console.log("+ unimplemented"); } },
+		{ 'symbol':'=', 'class':'greenButton', 'action':function() { calculator.equals(); } },
+		{ 'symbol':'X', 'class':'greenButton', 'action':function() { calculator.operatorPressed(function(a, b) { return a * b; }) } },
+		{ 'symbol':'/', 'class':'greenButton', 'action':function() { calculator.operatorPressed(function(a, b) { return a / b; }) } },
+		{ 'symbol':'-', 'class':'greenButton', 'action':function() { calculator.operatorPressed(function(a, b) { return a - b; }) } },
+		{ 'symbol':'+', 'class':'greenButton', 'action':function() { calculator.operatorPressed(function(a, b) { return a + b; }) } },
+		{ 'symbol':'+', 'class':'greenButton', 'action':function() { calculator.operatorPressed(function(a, b) { return a + b; }) } },
 		{ 'symbol':'M-', 'class':'greenButton', 'action':function() { console.log("M- unimplemented"); } },
-		{ 'symbol':'+/-', 'class':'greenButton', 'action':function() { console.log("+/- unimplemented"); } },
-		{ 'symbol':'9', 'class':'greenButton', 'action':function() { calculator.buttonPress('9'); } },
-		{ 'symbol':'6', 'class':'greenButton', 'action':function() { calculator.buttonPress('6'); } },
-		{ 'symbol':'3', 'class':'greenButton', 'action':function() { calculator.buttonPress('3'); } },
-		{ 'symbol':'.', 'class':'greenButton', 'action':function() { calculator.buttonPress('.'); } },
+		{ 'symbol':'+/-', 'class':'greenButton', 'action':function() { calculator.negate(); } },
+		{ 'symbol':'9', 'class':'greenButton', 'action':function() { calculator.numberPressed('9'); } },
+		{ 'symbol':'6', 'class':'greenButton', 'action':function() { calculator.numberPressed('6'); } },
+		{ 'symbol':'3', 'class':'greenButton', 'action':function() { calculator.numberPressed('3'); } },
+		{ 'symbol':'.', 'class':'greenButton', 'action':function() { calculator.numberPressed('.'); } },
 		{ 'symbol':'M+', 'class':'greenButton', 'action':function() { console.log("M+ unimplemented"); } },
-		{ 'symbol':'sqrt', 'class':'greenButton', 'action':function() { console.log("sqrt unimplemented"); } },
-		{ 'symbol':'8', 'class':'greenButton', 'action':function() { calculator.buttonPress('8'); } },
-		{ 'symbol':'5', 'class':'greenButton', 'action':function() { calculator.buttonPress('5'); } },
-		{ 'symbol':'2', 'class':'greenButton', 'action':function() { calculator.buttonPress('2'); } },
-		{ 'symbol':'0', 'class':'greenButton', 'action':function() { calculator.buttonPress('0'); } },
+		{ 'symbol':'sqrt', 'class':'greenButton', 'action':function() { calculator.sqrt(); } },
+		{ 'symbol':'8', 'class':'greenButton', 'action':function() { calculator.numberPressed('8'); } },
+		{ 'symbol':'5', 'class':'greenButton', 'action':function() { calculator.numberPressed('5'); } },
+		{ 'symbol':'2', 'class':'greenButton', 'action':function() { calculator.numberPressed('2'); } },
+		{ 'symbol':'0', 'class':'greenButton', 'action':function() { calculator.numberPressed('0'); } },
 		{ 'symbol':'MR/MC', 'class':'greenButton', 'action':function() { console.log("MC unimplemented"); } },
-		{ 'symbol':'AC', 'class':'greenButton', 'action':function() { console.log("AC unimplemented"); } },
-		{ 'symbol':'7', 'class':'greenButton', 'action':function() { calculator.buttonPress('7'); } },
-		{ 'symbol':'4', 'class':'greenButton', 'action':function() { calculator.buttonPress('4'); } },
-		{ 'symbol':'1', 'class':'greenButton', 'action':function() { calculator.buttonPress('1'); } },
-		{ 'symbol':'0', 'class':'greenButton', 'action':function() { calculator.buttonPress('0'); } },
+		{ 'symbol':'AC', 'class':'greenButton', 'action':function() { calculator.reset(); } },
+		{ 'symbol':'7', 'class':'greenButton', 'action':function() { calculator.numberPressed('7'); } },
+		{ 'symbol':'4', 'class':'greenButton', 'action':function() { calculator.numberPressed('4'); } },
+		{ 'symbol':'1', 'class':'greenButton', 'action':function() { calculator.numberPressed('1'); } },
+		{ 'symbol':'0', 'class':'greenButton', 'action':function() { calculator.numberPressed('0'); } },
 	];
 
 
@@ -70,6 +72,7 @@ boidem.adlessCalc = (function() {
             $('body').append('<div id="button' + i + '" class="' + definition.class + '">' + definition.symbol + '</div>');
             var button = $('#button' + i);
             button.css({"background-color":"green"});
+			button.css({'border-style': 'solid', 'border-width': '2px'});
             button.css({'position': 'absolute'});
 			button.css({'text-align':'center'});
 			button.click(definition.action);
@@ -116,19 +119,89 @@ boidem.adlessCalc = (function() {
 
 	var calculator = (function() {
 		var display;
+		var input;
+		var accumulator;
+		var operation;
+		var newInput;
+		var error;
 
 		var init = (function(displayTarget) {
 			display = displayTarget;
-			display.text('0');
+			input = '0';
+			accumulator = 0;
+			operation = null;
+			newInput = false;
+			error = false;
+			showInput();
 		});
 
-		var buttonPress = (function(value) {
-			NODEBUG || console.log("buttonPress: " + value);
+		var numberPressed = (function(value) {
+			NODEBUG || console.log("numberPressed: " + value);
+			if (newInput || (input === '0' && value !== '.'))
+				input = value;
+			else
+				input += value;
+			newInput = false;
+			showInput();
+		});
+
+		var operatorPressed = (function(value) {
+			var inputValue = parseFloat(input);
+			NODEBUG || console.log("operatorPressed: " + accumulator + value + inputValue);
+			if (operation)
+				accumulator = operation(accumulator, inputValue);
+			else
+				accumulator = inputValue;
+
+			operation = value;
+			input = accumulator;
+			newInput = true;
+			showInput();
+			NODEBUG || console.log("    result -> " + input);
+		});
+
+		var equals = (function() {
+			if (operation)
+				input = operation(accumulator, parseFloat(input));
+			newInput = true;
+			operation = null;
+			accumulator = 0;
+			showInput();
+		});
+
+		var negate = (function() {
+			if (input[0] == '-')
+				input = input.slice(1);
+			else
+				input = "-" + input;
+			showInput();
+		});
+
+
+		var sqrt = (function() {
+			equals();
+			var inputValue = parseFloat(input);
+			if (inputValue < 0) {
+				error = true;
+				input = '0';
+			}
+			else
+				input = Math.sqrt(parseFloat(input));
+			showInput();
+		});
+
+		var showInput = (function() {
+			display.text(input);
 		});
 
 		return {
 			init:function(displayTarget) { init(displayTarget); },
-			buttonPress:function(value) { buttonPress(value); }
+			numberPressed:function(value) { numberPressed(value); },
+			operatorPressed:function(value) { operatorPressed(value); },
+			equals:function() { equals(); },
+			negate:function() { negate(); },
+			sqrt:function() { sqrt(); },
+			reset:function() { init(display); },
 		};
 }());
 
